3850b348106c5026dae5c7723950976ff89299e3
Joao-Robert0 <80291396+Joao-Robert0@users.noreply.github.com>
2021-04-04 20:32:13 -0300

    Update 3-sat.h

--- 3-sat.h
+++ 3-sat.h
@@ -8,8 +8,9 @@ int numVar, numLine;
 void proximo(char *v, int VAR);
 void percorrer(int VAR);
 int verifica(char** mat,int linha, int col, char* clause);
-void printaLinha(char v, int col);
+void printaLinha(char v, int linha);
 void automatic();
 void manual();
+void atribui (int**mat, int*v)
 // Funcao que identifica o SO do computador do usuario
 void semPreconceito();
