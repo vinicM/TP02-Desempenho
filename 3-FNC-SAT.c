7c9a99bafe1a743d558382173f56ba4a5a5ea7a0
Artur Papa <artur.papa@ufv.br>
2021-04-07 17:03:46 -0300

    up

--- 3-FNC-SAT.c
+++ 3-FNC-SAT.c
@@ -5,169 +5,176 @@
 
 void semPreconceito()
 {
-    #ifdef _WIN32
-        system("cls");
-    #elif __linux__
-        system("clear");
-    #endif
-
 	#ifdef _WIN32
-		getch();
+		system("cls");
 	#elif __linux__
-		getchar();
+		system("clear");
 	#endif
 }
 
-void geraclauses(int (*mat)[50],int N){
-			int C;
-			int randomic1,randomic2,randomic3,test1,test2,test3;
-			int i,j;
-			C = (N/3)*2;
-
-   			for(i=0; i<C; i++){
-				for(j=0; j<N; j++)
-					mat[i][j] = 0;
-			}
-
-			srand(time(0));
-			for(i=0; i<C; i++){
-				int test1 = 0;
-				int test2 = 0;
-				int test3 = 0;
-
-
-				randomic1 = rand()%N;
-				test1 = rand()%3 ;
-				if(test1 == 0){
-				 	test1++;
-				}
-				mat[i][randomic1] = test1;
-				
-				
-				
-				randomic2 = rand()%N;
-				
-				test2 = rand()%3 ;
-				
-				if(test2 == 0){
-				 	test2++;
-				}
-				while(randomic2 == randomic1){
-					randomic2 = rand()%N;
-				}
-
-				mat[i][randomic2] = test2;
-			
-				
-				
-				randomic3 = rand()%N;
-				
-				test3 = rand()%3 ;
-				if(test3 == 0){
-				 	test3++;
-				}
-
-				while(randomic3 == randomic2 || randomic3 == randomic1){
-					randomic3 = rand()%N;
-				}
-				mat[i][randomic3] = test3;
-				
-			}
-			percorrer(mat,3,2,N,C);
-			//printa tabela geral
-			// for(i=0; i<C; i++){
-			// 	for(j=0; j<N; j++){
-			// 		printf("%d",mat[i][j]);
-					
-			// 	}printf("\n");
-			// }
-			
-}
-
-void proximo(char *v, int VAR, int VL) {
-    int i;
-    v[0]++;
-    for(i=0; i<VAR; i++) {
-        if(v[i] == VL) {
-            v[i] = 0;
-            v[i+1]++;
-        }
-    }
-}
-
-void printalinha(int* v, int colums){
-
-	int i,j;
-	int coluna1, coluna2, coluna3;
-	
-	for(i = 0; i<colums ; i++){
-		printf("%d",v[i]);
-	}
-}
-
-int verifica(char** mat, int linha, int col, char* clause){
-	int i, j, aux = 0;
-    int flag = 0;
-    for (i = 0; i < linha; i++)
-    {
-        for (j = 0; j < col; j++)
-        {    
-            if (mat[i][j] != 0)
-            {
-                clause[aux] = mat[i][j];
-                aux++;
-            }
-        }
-    }
-    for (i = 0; i < aux; i++)
-    {
-        if (clause[i] == 2)
-        {
-            flag = 1;
-        }
-    }
-    return flag;
-}
-
-int percorrer(int (*mat)[50], int VAR, int VL, int N, int C) {
-    char v[101];
-    for(int i=0; i<VAR; i++) v[i] = 0;
-    int i, j, comb=1;
-    for(int i=0; i<VAR; i++) comb *= VL;
+int percorrer(int (*mat)[50], int VAR, int VL, int N, int C)
+{
+	char v[101];
+	for (int i = 0; i < VAR; i++)
+		v[i] = 0;
+	int i, j, comb = 1;
+	for (int i = 0; i < VAR; i++)
+		comb *= VL;
 	int col;
 
 	printf(" TABELA ORIGINAL: \n");
-	for(i=0; i<C; i++){
+	for (i = 0; i < C; i++)
+	{
 		printf("(");
 		col = 0;
-		for(j=0; j<N; j++){
-			if(mat[i][j] != 0){
-				printf(" %sx%d %s", (mat[i][j] == 1 ? "!" : ""),(j+1),(col < 2 ? "|" : ")"));
+		for (j = 0; j < N; j++)
+		{
+			if (mat[i][j] != 0)
+			{
+				printf(" %sx%d %s", (mat[i][j] == 1 ? "!" : ""), (j + 1), (col < 2 ? "|" : ")"));
 				col++;
-			}	
+			}
 		}
 		printf("\n");
 	}
 
-
 	printf("TABELA DAS COMBINACOES:\n");
-    for(i=0; i<comb; i++) {
-        for(j=VAR-1; j>=0; j--) {
+	for (i = 0; i < comb; i++)
+	{
+		for (j = VAR - 1; j >= 0; j--)
+		{
 
-            printf("%d",v[j]);
-        }
-        // adicionar o resto das verificações
-        printf("\n");
-        proximo(v, VAR, VL);
-    }
-
-
-	
+			printf("%d", v[j]);
+		}
+		// adicionar o resto das verificações
+		printf("\n");
+		proximo(v, VAR, VL);
+	}
 }
 
-void escolhaMenu(){
+void geraclauses(int (*mat)[50], int N)
+{
+	int C;
+	int randomic1, randomic2, randomic3, test1, test2, test3;
+	int i, j;
+	C = (N / 3) * 2;
+
+	for (i = 0; i < C; i++)
+	{
+		for (j = 0; j < N; j++)
+			mat[i][j] = 0;
+	}
+
+	srand(time(0));
+	for (i = 0; i < C; i++)
+	{
+		int test1 = 0;
+		int test2 = 0;
+		int test3 = 0;
+
+		randomic1 = rand() % N;
+		test1 = rand() % 3;
+		if (test1 == 0)
+		{
+			test1++;
+		}
+		mat[i][randomic1] = test1;
+
+		randomic2 = rand() % N;
+
+		test2 = rand() % 3;
+
+		if (test2 == 0)
+		{
+			test2++;
+		}
+		while (randomic2 == randomic1)
+		{
+			randomic2 = rand() % N;
+		}
+
+		mat[i][randomic2] = test2;
+
+		randomic3 = rand() % N;
+
+		test3 = rand() % 3;
+		if (test3 == 0)
+		{
+			test3++;
+		}
+
+		while (randomic3 == randomic2 || randomic3 == randomic1)
+		{
+			randomic3 = rand() % N;
+		}
+		mat[i][randomic3] = test3;
+	}
+	percorrer(mat, 3, 2, N, C);
+	//printa tabela geral
+	// for(i=0; i<C; i++){
+	// 	for(j=0; j<N; j++){
+	// 		printf("%d",mat[i][j]);
+
+	// 	}printf("\n");
+	// }
+}
+
+void proximo(char *v, int VAR, int VL)
+{
+	int i;
+	v[0]++;
+	for (i = 0; i < VAR; i++)
+	{
+		if (v[i] == VL)
+		{
+			v[i] = 0;
+			v[i + 1]++;
+		}
+	}
+}
+
+void printalinha(int *v, int colums)
+{
+
+	int i, j;
+	int coluna1, coluna2, coluna3;
+
+	for (i = 0; i < colums; i++)
+	{
+		printf("%d", v[i]);
+	}
+}
+
+int verifica(char **mat, int linha, int col, char *clause)
+{
+	int i, j, aux = 0;
+	int flag = 0;
+	for (i = 0; i < linha; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			if (mat[i][j] != 0)
+			{
+				clause[aux] = mat[i][j];
+				aux++;
+			}
+		}
+	}
+	for (i = 0; i < aux; i++)
+	{
+		if (clause[i] == 2)
+		{
+			flag = 1;
+		}
+	}
+	return flag;
+}
+
+void escolhaMenu()
+{
 	int a;
-	while(1){
+	while (1)
+	{
 		printf("----------------------------------------------------------\n");
 		printf("----------------------------------------------------------\n");
 		printf("                                                          \n");
@@ -177,70 +184,68 @@ void escolhaMenu(){
 		printf("----------------------------------------------------------\n");
 		printf("----------------------------------------------------------\n");
 
-		a = getch();
-		//a = getchar();
+		scanf("%d", &a);
 
-			if(a == 27){
-				semPreconceito();
-				break;
-			}else if(a == 49){
-				semPreconceito();	
-								      
-			}else if(a == 50){
-				semPreconceito();
-				menuAutomatic();
-				break;
-			}
-	}
-
-}
-
-void menuManual(){
-
-    
-
-}
-
-
-void menuAutomatic(){
-		int VAR, VL = 2;
-		int clause[101][3];
-
-		printf("----------------------------------------------------------\n");
-		printf("----------------------------------------------------------\n");
-		printf("                                                          \n");
-		printf("            Escolha qual valor para N deseja  :           \n");
-		printf("     1 - 15        2 - 30        3 - 36        4 - 45     \n");
-		printf("                                                          \n");
-		printf("----------------------------------------------------------\n");
-		printf("----------------------------------------------------------\n");
-
-		int escolha;
-		scanf("%d", &escolha);
-		escolha = getch();
-		semPreconceito();
-
-		if (escolha == 49 || escolha == 1){
-			int mat[100][50];
-            int N,C;
-            N = 15;
-            C = (N/3)*2;
-            geraclauses(mat, N);
-			//percorrer(3,2);
-			//atribui(mat,N,C);
-		} 
-		if (escolha == 50 || escolha == 2){
-			
-			// 'a'+ rand%N
-
+		if (a == 0)
+		{
+			semPreconceito();
+			break;
 		}
-		
-
+		else if (a == 1)
+		{
+			semPreconceito();
+		}
+		else if (a == 2)
+		{
+			semPreconceito();
+			menuAutomatic();
+			break;
+		}
+	}
 }
 
-int main(){
-	
+void menuManual()
+{
+}
+
+void menuAutomatic()
+{
+	int VAR, VL = 2;
+	int clause[101][3];
+
+	printf("----------------------------------------------------------\n");
+	printf("----------------------------------------------------------\n");
+	printf("                                                          \n");
+	printf("            Escolha qual valor para N deseja  :           \n");
+	printf("     1 - 15        2 - 30        3 - 36        4 - 45     \n");
+	printf("                                                          \n");
+	printf("----------------------------------------------------------\n");
+	printf("----------------------------------------------------------\n");
+
+	int escolha;
+	scanf("%d", &escolha);
+	// escolha = getch();
+
+	if (escolha == 49 || escolha == 1)
+	{
+		int mat[100][50];
+		int N, C;
+		N = 15;
+		C = (N / 3) * 2;
+		geraclauses(mat, N);
+		//percorrer(3,2);
+		//atribui(mat,N,C);
+	}
+	if (escolha == 50 || escolha == 2)
+	{
+
+		// 'a'+ rand%N
+	}
+}
+
+int main()
+{
+
 	escolhaMenu();
 	return 0;
-
 }
